c0116250c7165c76cbe5ce0b3211cfcf507fa2cf
Ubuntu <ubuntu@ip-172-31-80-227.ec2.internal>
2019-09-17 17:01:12 +0000

    Second commit - Connect instance in aws with django, also do you could visit http://3.82.171.228/hello/

--- test1/settings.py
+++ test1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aip+)_cp&gi3z%33-oyc&@9sf@g(3as^-2el&t2cx8^w$h=q_#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.82.171.228']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
